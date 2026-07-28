89d117595ab87ce0a31c63b4f8745d8fc368eabd
Beigers <bacuna00938@ufide.ac.cr>
2026-07-28 15:56:58 -0600

    Creaccion de las clases

--- src/main/java/com/mycompany/proyecto/progra/ProyectoProgra.java
+++ src/main/java/com/mycompany/proyecto/progra/ProyectoProgra.java
@@ -11,6 +11,6 @@ package com.mycompany.proyecto.progra;
 public class ProyectoProgra {
 
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        
     }
 }
